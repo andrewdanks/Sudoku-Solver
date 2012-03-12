96450678596d8c98960c88e2aab79db567458c83
Andrew Danks <a.danks@gmail.com>
2012-03-12 01:36:03 -0400

    Init.

--- sudoku_csp.py
+++ sudoku_csp.py
@@ -1,4 +1,75 @@
 from cspbase import *
+import itertools
+import sys
+from collections import deque
+
+SQUARE_SIZE = 9
+
+def permutations(L):
+  '''Return a list of all permutations of list L.'''
+  return list(itertools.permutations(L))
+
+def intersection(L1, L2):
+  '''Return the list representing the intersection of lists L1 and L2.'''
+  ret = set(L1).intersection(set(L2))
+  return list(ret)
+
+def get_box_number(row, col):
+  '''Return what box (row,col) is in. Boxes numbers 0 to 8 from left to right, top to bottom'''
+  quadrants = {0:range(3),1:range(3,6),2:range(6,9)}
+  for (i,q) in quadrants.items():
+    if row in q: row_quadrant = i
+  for (i,q) in quadrants.items():
+    if col in q: col_quadrant = i
+  return 3*row_quadrant + col_quadrant
+
+def get_cell_values_for_box(box, board):
+  '''Return a list of the cells that are in the given box (an int from 0-8). Each cell in the list
+  is a dictionary of the form {'row':int, 'col':int}'''
+  cells = []
+  for row in range(9):
+    for col in range(9):
+      if get_box_number(row,col) == box:
+        cells.append(board[row][col])
+  return cells
+
+def get_cells_for_box(box):
+  '''Return a list of the cells that are in the given box (an int from 0-8). Each cell in the list
+  is a dictionary of the form {'row':int, 'col':int}'''
+  cells = []
+  for row in range(9):
+    for col in range(9):
+      if get_box_number(row,col) == box:
+        cells.append({'row':row,'col':col})
+  return cells
+
+
+def fill_list(L, fill_list):
+  '''Given list L, fill each 0 in the list with the elements from fill_list.'''
+  new_L = L[:]
+  j = 0
+  for i in range(len(L)):
+    if L[i] == 0:
+      new_L[i] = fill_list[j]
+      j += 1
+  return new_L
+
+def cell_id(row,col):
+  return str(row)+str(col)
+
+
+def write_board_variables(variables):
+  '''Put the variables of the board in a readable format.'''
+
+  human_board = []
+  for row in range(SQUARE_SIZE):
+    human_board.append([])
+    for col in range(SQUARE_SIZE):
+      V = variables[row][col]
+      domain = V.cur_domain()
+      domain.sort()
+      human_board[row].append(domain)
+  return human_board
 
 
 def enforce_gac(constraint_list):
@@ -8,43 +79,13 @@ def enforce_gac(constraint_list):
      The pruned values will be reflected in the variable object cur_domain (i.e.,
      enforce_gac will modify the variable objects that are in the scope of
      the constraints passed to it.'''
-
-  GACQueue = constraint_list
-
-  while not GACQueue.empty():
-
-    C = GACQueue.pop(0)
-
-    i = 0
+  GACQueue = deque(constraint_list)
+  while GACQueue:
+    C = GACQueue.popleft()
     for V in C.scope:
-      j = 0
       for d in V.cur_domain():
-
-        # Find set of assignments A for all other variables in C.scope
-        # such that C(A U V=d) is True
-        #found_A = False
-
-        #for V2 in C.scope:
-        #  if V2 is V: continue
-
-        #  t = []
-        #  for x in V2:
-        #    t.append(x)
-        found_A = find_set_of_assignments(C, V, d, scope)
-
-        variables = S.scope
-        variables.remove(V)
-        seen = [V]
-        Q = [V]
-        while not Q.empty():
-          t = Q.pop(0)
-          if 
-
-
-
-
-
-        if not found_A:
+        if not C.has_support(V, d):
+          #print 'pruned', d, 'from', V.name
           V.prune_value(d)
           if V.domain_size() < 1:
             return False
@@ -54,136 +95,248 @@ def enforce_gac(constraint_list):
             for C2 in constraint_list:
               if C2 not in GACQueue and V in C2.scope:
                 GACQueue.append(C2)
-
-        j += 1
-      i += 1
-
-
-
   return True
-                            
-def sudoku_enforce_gac_model_1(initial_sudoku_board):
-  '''The input board is specified as a list of 9 lists. Each of the
-     9 lists represents a row of the board. If a 0 is in the list it
-     represents an empty cell. Otherwise if a number between 1--9 is
-     in the list then this represents a pre-set board position.
 
-     In model_1 you should create a variable for each cell of the
-     board, with domain equal to {1-9} if the board has a 0 at that
-     position, and domain equal {i} if the board has a fixed number i
-     at that cell. 
-     
-     Model_1 should create BINARY CONSTRAINTS OF NOT-EQUAL between all
-     relevant variables (e.g., all pairs of variables in the same
-     row), then invoke enforce_gac on those constraints. All of the
-     constraints of Model_1 MUST BE binary constraints (i.e.,
-     constraints whose scope includes two and only two variables).
-     
-     The ouput should have the same layout as the input: a list of
-     nine lists each representing a row of the board. However, now the
-     numbers in the positions of the input list are to be replaced by
-     LISTS which are the corresponding cell's pruned domain (current
-     domain) AFTER gac has been performed.
-     
-     For example, if GAC failed to prune any values the output from
-     the above input would result in an output would be: NOTE I HAVE
-     PADDED OUT ALL OF THE LISTS WITH BLANKS SO THAT THEY LINE UP IN
-     TO COLUMNS. Python would not output this list of list in this
-     format.
-     
-     [[[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                2],[1,2,3,4,5,6,7,8,9],[                9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                6],[1,2,3,4,5,6,7,8,9]],
-     [[1,2,3,4,5,6,7,8,9],[                4],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                1],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                8]],
-     [[1,2,3,4,5,6,7,8,9],[                7],[1,2,3,4,5,6,7,8,9],[                4],[                2],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                3]],
-     [[                5],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                3],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]],
-     [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                1],[1,2,3,4,5,6,7,8,9],[                6],[1,2,3,4,5,6,7,8,9],[                5],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]],
-     [[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                3],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                6]],
-     [[                1],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                5],[                7],[1,2,3,4,5,6,7,8,9],[                4],[1,2,3,4,5,6,7,8,9]],
-     [[                6],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                2],[1,2,3,4,5,6,7,8,9]],
-     [[1,2,3,4,5,6,7,8,9],[                2],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[                8],[1,2,3,4,5,6,7,8,9],[                1],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]]
-     
-     Of course, GAC would prune some variable domains so this would
-     not be the outputted list.
-     '''
+def initialize_variables(board):
+
+  # Create a variable for each cell of the board:
+  variables = {}  #indexed by row then col
+  variables_by_col = {} # indexed by col then row
+  variables_by_box = {} # indexed by box
+  for row in range(SQUARE_SIZE):
+    variables[row] = {}
+    for col in range(SQUARE_SIZE):
+      d = board[row][col]
+      domain = range(1,10) if d == 0 else [d]
+      V = Variable('('+str(row)+','+str(col)+')', domain)
+      variables[row][col] = V
+
+      box = get_box_number(row, col)
+
+      if box not in variables_by_box:
+        variables_by_box[box] = []
+      variables_by_box[box].append(V)
+
+      if col not in variables_by_col:
+        variables_by_col[col] = {}
+      variables_by_col[col][row] = V
+
+  return variables, variables_by_col, variables_by_box
 
 
-'''
-X Dom[X] = {1,2,3}
-Y Dom[Y] = {3,4,5}
-Z Dom[Z] = {4,6,8}
-C(X,Y,Z) = True iff Z=X+Y
-constraint: [ [1 3 4] [1 5 6] [2 4 6] [3 3 6] [3 5 8] ]
-'''
+def sudoku_enforce_gac_model_1(board):
+  SQUARE_SIZE = len(board)
 
-    variables = {}
-    for row in initial_sudoku_board:
-      variables[row] = {}
-      for col in row:
-        val = initial_sudoku_board[row][col]
-        domain = range(1,10) if val == 0 else [val]
-        variables[row][col] = Variable(str(row)+','str(col), domain)
+  variables, variables_by_col, variables_by_box = initialize_variables(board)
+  constraint_list = []
 
-    sat_tuples = []
-    constraints = []
-    for row in variables:
-      scope = []
-      for col in row:
+  compared = {}
 
-        V = variables[row][col]
-        domain = V.domain()
-        if len(domain) == 1:
-          prune_val = domain[0]
-          for r in variables:
-            for c in r:
-              if r == row and c == col: continue
-              V2 = variables[r][c]
-              V2.prune_value(prune_val)
-        scope.append(V)
-      C = Constraint('NOT-EQUAL-ROW-'+str(row), scope)
-      constraints.append(c)
+  # Create all binary not-equal constraints for values in the same row:
+  for row in range(SQUARE_SIZE):
+    for col1 in range(SQUARE_SIZE):
+      CELL1_ID = cell_id(row,col1)
+      if CELL1_ID not in compared: compared[CELL1_ID] = []
 
-    # Generate satisfying tuples:
+      for col2 in range(SQUARE_SIZE):
 
+        CELL2_ID = cell_id(row,col2)
+        if (CELL1_ID == CELL2_ID) or (CELL2_ID in compared and CELL1_ID in compared[CELL2_ID]) or CELL2_ID in compared[CELL1_ID]:
+          continue
+        compared[CELL1_ID].append(CELL2_ID)
+        
+        V1, V2 = variables[row][col1], variables[row][col2]
+        scope = [V1, V2]
+        C = Constraint('NOT-EQUAL-ROW:'+V1.name+';'+V2.name, scope)
 
-    for c in constraints: c.print_constraint()
-    enforce_gac(constraints)
-    for c in constraints: c.print_constraint()
+        sat_tuples = []
+
+        for d1 in V1.cur_domain():
+          for d2 in V2.cur_domain():
+            if d1 != d2:
+              t1, t2 = [d1, d2], [d2, d1]
+              if C.tuple_is_valid(t1):
+                sat_tuples.append(t1)
+              if C.tuple_is_valid(t2):
+                sat_tuples.append(t2)
+
+        C.add_satisfying_tuples(sat_tuples)
+        constraint_list.append(C)
 
 
+  # Create all binary not-equal constraints for values in the same col:
+  for col in range(SQUARE_SIZE):
+    for row1 in range(SQUARE_SIZE):
+      CELL1_ID = cell_id(row1,col)
+      if CELL1_ID not in compared: compared[CELL1_ID] = []
+
+      for row2 in range(SQUARE_SIZE):
+
+        CELL2_ID = cell_id(row2,col)
+        if (CELL1_ID == CELL2_ID) or (CELL2_ID in compared and CELL1_ID in compared[CELL2_ID]) or CELL2_ID in compared[CELL1_ID]:
+          continue
+        compared[CELL1_ID].append(CELL2_ID)
+        
+        V1, V2 = variables_by_col[col][row1], variables_by_col[col][row2]
+        scope = [V1, V2]
+        C = Constraint('NOT-EQUAL-COL:'+V1.name+';'+V2.name, scope)
+
+        sat_tuples = []
+
+        for d1 in V1.cur_domain():
+          for d2 in V2.cur_domain():
+            if d1 != d2:
+              t1, t2 = [d1, d2], [d2, d1]
+              if C.tuple_is_valid(t1):
+                sat_tuples.append(t1)
+              if C.tuple_is_valid(t2):
+                sat_tuples.append(t2)
+
+        C.add_satisfying_tuples(sat_tuples)
+        constraint_list.append(C)
 
 
+  # Create all binary not-equal constraints for values in the same box:
+  for box in range(SQUARE_SIZE):
+    cells = get_cells_for_box(box)
+    for cell1 in cells:
+      row1, col1 = cell1['row'], cell1['col']
+      CELL1_ID = cell_id(row1,col1)
+      if CELL1_ID not in compared: compared[CELL1_ID] = []
 
-##############################
+      for cell2 in cells:
+        row2, col2 = cell2['row'], cell2['col']
+        CELL2_ID = cell_id(row2,col2)
 
-def sudoku_enforce_gac_model_2(initial_sudoku_board):
-  '''This function takes the same input format (a list of 9 lists
-  specifying the board, and generates the same format output as
-  sudoku_enforce_gac_model_1.
+        if (CELL1_ID == CELL2_ID) or (CELL2_ID in compared and CELL1_ID in compared[CELL2_ID]) or CELL2_ID in compared[CELL1_ID]:
+          continue
+        compared[CELL1_ID].append(CELL2_ID)
+
+        
+        V1, V2 = variables[row1][col1], variables[row2][col2]
+        scope = [V1, V2]
+        C = Constraint('NOT-EQUAL-BOX:'+V1.name+';'+V2.name, scope)
+
+        sat_tuples = []
+
+        for d1 in V1.cur_domain():
+          for d2 in V2.cur_domain():
+            if d1 != d2:
+              t1, t2 = [d1, d2], [d2, d1]
+              if C.tuple_is_valid(t1):
+                sat_tuples.append(t1)
+              if C.tuple_is_valid(t2):
+                sat_tuples.append(t2)
+
+        C.add_satisfying_tuples(sat_tuples)
+        constraint_list.append(C)
+
+
+  enforce_gac(constraint_list)
+
+  new_board = write_board_variables(variables)
+  for row in new_board:
+    print row
+
+
+def model1_create_constraint(name, variables, relevant_board, cells, compared):
+  for cell1 in cells:
+
+    row1, col1 = cell1['row'], cell1['col']
+    CELL1_ID = cell_id(row1,col1)
+
+    if CELL1_ID not in compared:
+      compared[CELL1_ID] = []
+
+    for cell2 in cells:
+
+      row2, col2 = cell2['row'], cell2['col']
+      CELL2_ID = cell_id(row2,col2)
+
+      if (CELL1_ID == CELL2_ID) or (CELL2_ID in compared and CELL1_ID in compared[CELL2_ID]) or CELL2_ID in compared[CELL1_ID]:
+          continue
+
+      compared[CELL1_ID].append(CELL2_ID)
+
+      V1, V2 = variables[row1][col1], variables[row2][col2]
+      scope = [V1, V2]
+
+      C = Constraint(name, scope)
+
+      sat_tuples = []
+
+      for d1 in V1.cur_domain():
+        for d2 in V2.cur_domain():
+          if d1 != d2:
+            t1, t2 = [d1, d2], [d2, d1]
+            if C.tuple_is_valid(t1):
+              sat_tuples.append(t1)
+            if C.tuple_is_valid(t2):
+              sat_tuples.append(t2)
+
+      C.add_satisfying_tuples(sat_tuples)
+      constraint_list.append(C)
+
+      return C
+
+def sudoku_enforce_gac_model_2(board):
+  SQUARE_SIZE = len(board)
+
+  board_by_col = []
+  for col in range(SQUARE_SIZE):
+    board_by_col.append([])
+    for row in range (SQUARE_SIZE):
+      board_by_col[col].append([])
+    for row in range(SQUARE_SIZE):
+      board_by_col[col][row] = board[row][col]
+
+
+  variables, variables_by_col, variables_by_box = initialize_variables(board)
+  constraint_list = []
+
+  # Create ALL-DIFF constraints for each row:
+  for row in range(SQUARE_SIZE):
+    C = model2_create_constrant('ALL-DIFF-ROW-'+str(row), variables, board[row], row)
+    constraint_list.append(C)
+
+  # Create ALL-DIFF constraints for each col:
+  for col in range(SQUARE_SIZE):
+    C = model2_create_constrant('ALL-DIFF-COL-'+str(col), variables_by_col, board_by_col[col], col)
+    constraint_list.append(C)
+
+
+  # Create ALL-DIFF constraints for each col:
+  for box in range(SQUARE_SIZE):
+    relevant_board = get_cell_values_for_box(box, board)
+    C = model2_create_constrant('ALL-DIFF-BOX-'+str(box), variables_by_box, relevant_board, box)
+    constraint_list.append(C)
+
+
+  enforce_gac(constraint_list)
+
+
+  new_board = write_board_variables(variables)
+  for row in new_board:
+    print row
+
+
+def model2_create_constrant(name, variables, relevant_board, i):
+  scope = []
+  for j in range(SQUARE_SIZE):
+    scope.append(variables[i][j])
+  C = Constraint(name, scope)
+  sat_tuples = []
+  free_values = []
+  for d in range(1,10):
+    if d not in relevant_board:
+      free_values.append(d)
+  free_values_comb = permutations(free_values)
+  for f in free_values_comb:
+    t = fill_list(relevant_board, f)
+    if C.tuple_is_valid(t):
+      sat_tuples.append(t)
+  C.add_satisfying_tuples(sat_tuples)
   
-  The variables of model_2 are the same as for model_1: a variable
-  for each cell of the board, with domain equal to {1-9} if the
-  board has a 0 at that position, and domain equal {i} if the board
-  has a fixed number i at that cell.
+  return C
 
-  However, model_2 has different constraints. In particular, instead
-  of binary non-equals constaints model_2 has 27 all-different
-  constraints: all-different constraints for the variables in each
-  of the 9 rows, 9 columns, and 9 sub-squares. Each of these
-  constraints is over 9-variables (some of these variables will have
-  a single value in their domain). model_2 should create these
-  all-different constraints between the relevant variables, then
-  invoke enforce_gac on those constraints.
-  '''
 
-  variables = {}
-  for row in initial_sudoku_board:
-  variables[row] = {}
-  for col in row:
-    val = initial_sudoku_board[row][col]
-    domain = [val] if val != 0 else range(1,10)
-    v = Variable(str(row)+','str(col), domain)
-    variables[row][col] = v
-   
-  C_BOX = Constraint('NOT-EQUAL-BOX')
-  C_COL = Constraint('NOT-EQUAL-COL')
-  C_ROW = Constraint('NOT-EQUAL-ROW')
-    
